bc85bbe6c3fd10b8198996e3763495349e15d9cd
Samuel Osei Adu <asamuel355@yahoo.com>
2022-04-29 15:43:26 +0000

    updates

--- sendMessage.php
+++ sendMessage.php
@@ -24,6 +24,31 @@
         if($insert){
             //send message on live server
             echo 'success';
+            $to = 'info@godaveplus.com';
+                    
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
+            $headers .= "From: Possible client <".$email."> " . "\r\n" .
+            "Reply-To: ".$email."" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+            
+            $subject = "Godave Plus Company LTD. ";
+            
+            
+            $messageSend = " 
+            <html>
+                <body>
+                    <h4> Message from Godave plus website </h4> <br>
+                    Full Name:  ".$firstname. ' '. $lastname."<br>
+                    Email: ".$email."<br>
+                    Phone : ".$phone." <br> <br><br>
+                    <h4> Message</h4> <br>
+                    ".$message."
+                </body>
+            </html> 
+            "; 
+
+            mail ($to, $subject, $messageSend, $headers);
         }else{
             echo 'Sorry your message could not send. Try again later';
         }
